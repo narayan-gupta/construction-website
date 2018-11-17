0afd7a324a813c278f82c20ee96f8a5f0f84dfaf
sharma0611 <sharma.shivam0611@gmail.com>
2018-11-17 17:34:46 +0000

    added endpoints

--- construction/core/urls.py
+++ construction/core/urls.py
@@ -3,5 +3,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-        path('', views.homepage, name='homepage'),
-        ]
+    path('', views.homepage, name='homepage'),
+    path('services', views.services, name='services'),
+    path('careers', views.careers, name='careers'),
+    path('planroom', views.planroom, name='planroom'),
+    path('contact-us', views.contact, name='contact'),
+]
--- construction/core/views.py
+++ construction/core/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def homepage(request):
+    return render(request, 'index.html')
+
+
+def services(request):
+    return render(request, 'index.html')
+
+
+def careers(request):
+    return render(request, 'index.html')
+
+
+def planroom(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
     return render(request, 'index.html')
